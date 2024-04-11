7b8a3cd0b9009d42ca90f812c28b6636b396bb80
Columbia Team14 <columbiateam14@gmail.com>
2024-04-11 03:38:03 +0000

    Built base model returns

---
+++ models/base/google_drive/base_google_drive__returns.sql
@@ -0,0 +1,8 @@
+SELECT _FILE,
+       _LINE,
+       RETURNED_AT as RETURNED_DATE,
+       ORDER_ID,
+       IS_REFUNDED,
+       _MODIFIED as _MODIFIED_TZ,
+       _FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_TS,
+FROM {{ source('google_drive', 'returns') }} 
